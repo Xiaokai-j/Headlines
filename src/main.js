cbcc4d28b1f4808d6cd504b4a6d5761abd2623c0
King <itheima@itcast.cn>
2020-02-29 15:42:19 +0800

    准备资源

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui' // 引入模块
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
+import '@/styles/index.less' // 引入样式
 Vue.config.productionTip = false
 Vue.use(ElementUI)// 全局注册element-ui框架 一旦全局注册 所有的位置都可以使用组件
 // Vue.use实际上 会调用element-ui 里面的install方法
--- src/router/index.js
+++ src/router/index.js
@@ -6,18 +6,24 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    // 强制跳转
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/home'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    name: 'Home',
+    component: Home
   }
+//   {
+//     // 按需加载
+//     path: '/about',
+//     name: 'About',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+//   }
 ]
 
 const router = new VueRouter({
